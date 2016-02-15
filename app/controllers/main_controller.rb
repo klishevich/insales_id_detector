26248a622ad15085d1f25e7ae2f147b1c6abe0b6
klishevich <busation@gmail.com>
2016-02-16 01:25:16 +0300

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,7 +6,7 @@ class MainController < ApplicationController
   end
 
   def installjs
-  	  my_app = self.current_app
+  	  #my_app = self.current_app
 
 	  # uri = URI.parse("http://busation.myinsales.ru/admin/js_tags.xml")
 	  # req = Net::HTTP.new(uri.hostname, uri.port)
@@ -18,6 +18,7 @@ class MainController < ApplicationController
 	  # base64user_and_pass = "4e95366ab96fe339e38123732ea08570"
 	  # res = req.post(uri.path, xml_data, {'Content-Type' => 'text/xml', 'Content-Length' => xml_data.length.to_s, 'Authorization' => "Basic #{base64user_and_pass}", "Connection" => "keep-alive" })
 	  # @res1 = res.body
-	  @res1 = self.current_app
+	  @res1 = @account.insales_subdomain
+	  @res2 = 'test'
   end
 end
