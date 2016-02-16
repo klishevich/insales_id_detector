e73bc7bbf9888b10d3b444c72b2d94da61956590
klishevich <busation@gmail.com>
2016-02-16 14:29:34 +0300

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -15,14 +15,21 @@ class MainController < ApplicationController
   	  Rails.logger.info(my_pass)
   	  Rails.logger.info(' my_url: ')
   	  Rails.logger.info(my_url)
-	  uri = URI.parse(my_url)
-	  req = Net::HTTP.new(uri.hostname, uri.port)
+	  # uri = URI.parse(my_url)
+	  # req = Net::HTTP.new(uri.hostname, uri.port)
 	  xml_data = %{<?xml version="1.0" encoding="UTF-8"?>
 	  <js-tag>
     	<type type="string">JsTag::FileTag</type>
     	<content>http://www.busation.com/assets/application-22d02ec7221607ea6e60cfb943c49d0a.js</content>
 	  </js-tag>}
-	  res = req.post(uri.path, xml_data, {'Content-Type' => 'text/xml', 'Content-Length' => xml_data.length.to_s, 'Authorization' => "Basic #{my_pass}", "Connection" => "keep-alive" })
-	  Rails.logger.info(res)
+	  # res = req.post(uri.path, xml_data, {'Content-Type' => 'text/xml', 'Content-Length' => xml_data.length.to_s, 'Authorization' => "Basic #{my_pass}", "Connection" => "keep-alive" })
+	  # Rails.logger.info(res)
+
+	  uri = my_url
+	  request = Net::HTTP::Post.new uri.path
+	  request.body = xml_data
+	  request.content_type = 'text/xml'
+	  response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
+	  Rails.logger.info(response.body)
   end
 end
