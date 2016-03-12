ebfd518fcdb8b3215d3d593c268745eea58e20cd
klishevich <busation@gmail.com>
2016-03-12 17:01:34 +0300

    write to file

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -16,7 +16,7 @@ class MainController < ApplicationController
  #  	code_doc.css("script").each do |node|
  #  		src_value = node["src"]
 	# end
-	src_value = code_doc.xpath('//noindex').first.children.last.to_s
+	  src_value = code_doc.xpath('//noindex').first.children.last.to_s
   	Rails.logger.info(' src_value: ')
   	Rails.logger.info(src_value)
   	str_index = src_value.index('base64')
@@ -28,6 +28,13 @@ class MainController < ApplicationController
   	src_value3 = Base64.decode64(src_value2)
    	Rails.logger.info(' src_value3: ')
   	Rails.logger.info(src_value3)
+    file_name = "id"+Time.now.strftime("%Y%m%d%H%M")+".js"
+    file = File.join(Rails.root, 'public', 'system', 'file_name')
+    Rails.logger.info(' file: ')
+    Rails.logger.info(file)
+    output = File.open( file, "w" )
+    output << src_value3
+    output.close
 
   	# esc_code = src_value3.encode(:xml => :attr)
   	# Rails.logger.info(' esc_code: ')
@@ -41,22 +48,27 @@ class MainController < ApplicationController
   	Rails.logger.info(my_pass)
   	Rails.logger.info(' my_url: ')
   	Rails.logger.info(my_url)
-	xml_data = %{<?xml version="1.0" encoding="UTF-8"?>
-	  <js-tag>
-		<type type="string">JsTag::TextTag</type>
-		<content>} + src_value3 + %{</content>
-	  </js-tag>}
-	uri = URI.parse(my_url)
-	request = Net::HTTP::Post.new uri.path
-	Rails.logger.info(' Request body: ')
-	request.body = xml_data
-	Rails.logger.info(xml_data)
-	request.content_type = 'text/xml'
-	request.basic_auth 'id_detector', my_pass
-	response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
-	Rails.logger.info( 'response body:' )
-	Rails.logger.info(response.body)
-	redirect_to '/installjs'
+
+    js_file_url = "http://id-detector.j123.ru/system/" + file_name
+    Rails.logger.info(' js_file_url: ')
+    Rails.logger.info(js_file_url)
+
+	  xml_data = %{<?xml version="1.0" encoding="UTF-8"?>
+	    <js-tag>
+		  <type type="string">JsTag::FileTag</type>
+		  <content>} + src_value3 + %{</content>
+	    </js-tag>}
+	  uri = URI.parse(my_url)
+	  request = Net::HTTP::Post.new uri.path
+	  Rails.logger.info(' Request body: ')
+	  request.body = xml_data
+	  Rails.logger.info(xml_data)
+	  request.content_type = 'text/xml'
+	  request.basic_auth 'id_detector', my_pass
+	  response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
+	  Rails.logger.info( 'response body:' )
+	  Rails.logger.info(response.body)
+	  redirect_to '/installjs'
   end
 
   def installjs
